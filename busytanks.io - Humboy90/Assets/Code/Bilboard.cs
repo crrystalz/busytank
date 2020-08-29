dccc112a55ce6c3fa17970f890c7257a4b1fa0a7
Humboy90 <69700181+Humboy90@users.noreply.github.com>
2020-08-28 18:55:32 -0700

    Test
    test

---
+++ busytanks.io - Humboy90/Assets/Code/Bilboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bilboard : MonoBehaviour
+{
+
+    public Transform cam;
+    // Start is called before the first frame update
+
+    
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
---
+++ busytanks.io - Humboy90/Assets/Code/PlayerSCRIPT.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSCRIPT : MonoBehaviour
+{
+    public int maxHP = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHP;
+        healthBar.SetMaxHP(maxHP);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage (int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHP(currentHealth);
+    }
+}
