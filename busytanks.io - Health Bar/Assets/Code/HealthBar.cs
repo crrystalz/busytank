fc454a33ffbade943024695c5623655c25337c64
Humboy90 <69700181+Humboy90@users.noreply.github.com>
2020-09-01 20:01:58 -0700

    Health Bar
    I am still fixing it.

---
+++ busytanks.io - Health Bar/Assets/Code/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient Color;
+    public Image fill;
+    public void SetMaxHP(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = Color.Evaluate(1f);
+    }
+    public void SetHP(int health)
+    {
+        slider.value = health;
+
+        fill.color = Color.Evaluate(slider.normalizedValue);
+    }
+}
