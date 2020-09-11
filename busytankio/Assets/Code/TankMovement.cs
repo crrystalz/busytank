a50bccc7b08caf2aa6a486a32a5a6716c7df7feb
Alexander Kang <alexander7dk@gmail.com>
2020-09-10 22:42:30 -0700

    Made the tank movement

---
+++ busytankio/Assets/Code/TankMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
