51722d6e5338e9bd41931cc96ae6e99baff26f02
Alexander Kang <alexander7dk@gmail.com>
2020-09-21 22:44:14 -0700

    Finished making Tank attributes

--- busytankio/Assets/Scripts/TankMovement.cs
+++ busytankio/Assets/Scripts/TankMovement.cs
@@ -6,18 +6,52 @@ public class TankMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 6f;
+    // public float speed = 6f;
     public bool shildUp = false;
     public bool ammoUp = false;
     public bool healthUp = false;
     // public string tankCategory = "Scout Tank";
     public enum TankCategory {HeavyTank, ScoutTank, MortarTank, SniperTank};
     public TankCategory tankType;
+    private int health = 100;
+    private int attack = 100;
+    private int speed = 100;
+    private int zoom = 100;
+    private int view = 100;
 
     Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        switch(tankType)
+        {
+            case TankCategory.HeavyTank:
+                Debug.Log("Setting HeavyTank Properties");
+                health = 120;
+                speed = 70;
+            break;
+
+            case TankCategory.ScoutTank:
+                Debug.Log("Setting ScoutTank Properties");
+                attack = 90;
+                speed = 110;
+                zoom = 104;
+            break;
+
+            case TankCategory.MortarTank:
+                Debug.Log("Setting MortarTank Properties");
+                attack = 130;
+                speed = 60;
+                view = 108;
+            break;
+            
+            case TankCategory.SniperTank:
+                Debug.Log("Setting SniperTank Properties");
+                health = 70;
+                attack = 125;
+                zoom = 130;
+            break;
+        }
     }
     void Update()
     {
@@ -27,7 +61,7 @@ public class TankMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+            controller.Move(direction * speed / 20 * Time.deltaTime);
         }
     }
     private void OnTriggerEnter(Collider other)
