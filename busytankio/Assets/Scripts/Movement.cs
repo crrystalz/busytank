5d07fe0917141535a3d01def1449d423edf83b88
Rrishi Anand <lichupatnaik@gmail.com>
2020-09-26 13:01:30 -0700

    OMG SO MANY BIG CHANGES
    REDID COMPLETELY MOVEMENT ACCORDING TO A NEW TUTORIAL AND FIXED LIKE A BAZILLION ERRORS

---
+++ busytankio/Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float turnspeed;
+    public float gravitymultiplier;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // IMPORTANT!!! RIGHT = FORWARD  LEFT = BACKWARD !!!!!
+
+        Move();
+        Turn();
+      
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(Vector3.right * speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddRelativeForce(-Vector3.right * speed);
+        }
+        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
+        localVelocity.z = 0;
+        rb.velocity = transform.TransformDirection(localVelocity);
+
+    }
+
+    void Turn()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(Vector3.up * turnspeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(-Vector3.up * turnspeed);
+        }
+    }
+}
