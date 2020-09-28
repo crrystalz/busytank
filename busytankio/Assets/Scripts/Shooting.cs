ce4a2375959824c4cf325d3f5be7ee3dcad340b9
Rrishi Anand <lichupatnaik@gmail.com>
2020-09-27 18:03:26 -0700

    Started Working On Shooting Tutorial

---
+++ busytankio/Assets/Scripts/Shooting.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+        void Shoot() 
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(GameObject.Find("Player").transform.position, GameObject.Find("Player").transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}
