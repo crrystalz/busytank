aa4b984853477f3daf94e151b73af1f9fcdaadfa
Rrishi Anand <lichupatnaik@gmail.com>
2020-12-05 12:08:52 -0800

    Shooting Rework & More
    Deleted all the old shooting stuff, and re-did it using the Raycast system, additionally the targets will dissapear after being hit repeatedly as a result of a health system I implemented.

---
+++ busytankio/Assets/Scripts/Gun.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(player.transform.position, player.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ busytankio/Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public GameObject TargetObject;
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Debug.Log(TargetObject.name + " has been destructed");
+            Die();
+        }
+    }
+
+    void Die ()
+    {
+        Destroy(gameObject);
+    }
+}
