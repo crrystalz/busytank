6e0ecdeb9b39337eede9fac44343dbdeeeb3bb9c
Humboy90 <quinnkass@gmail.com>
2021-01-12 19:51:23 -0500

    net

--- busytankio/Assets/Scripts/NetworkController.cs
+++ busytankio/Assets/Scripts/NetworkController.cs
@@ -7,33 +7,12 @@ using UnityEngine;
 public class NetworkController : MonoBehaviourPunCallbacks
 {
 
-    [SerializeField]
-    private int roomsize;
+    
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
-        PhotonNetwork.JoinRandomRoom();
-        Debug.Log("Finding a match...");
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("Connected!");
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        Debug.Log("No room open, createing a match.");
-        CreateRoom();
-    }
-
-    void CreateRoom()
-    {
-        int randomRoomNumber = Random.Range(0, 10000);
-        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomsize };
-        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
-        Debug.Log(randomRoomNumber);
+        
     }
 
     // Update is called once per frame
