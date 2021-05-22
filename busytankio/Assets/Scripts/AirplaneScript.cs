c3e276ae3c0c41caa4c0a27da03302b88258a147
Humboy90 <quinnkass@gmail.com>
2021-05-22 13:15:36 -0400

    stuff

---
+++ busytankio/Assets/Scripts/AirplaneScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirplaneScript : MonoBehaviour
+{
+    // Player Movement Variables/....
+    public static int movespeed = 1;
+    public Vector3 userDirection = Vector3.right;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+    }
+}
