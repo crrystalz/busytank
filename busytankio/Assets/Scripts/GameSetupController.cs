95ad56b9d2121e437c459605e4788b58e7f84c54
Mytheral <quinnkass@gmail.com>
2021-08-23 20:39:09 -0700

    logos
    {insert text}

--- busytankio/Assets/Scripts/GameSetupController.cs
+++ busytankio/Assets/Scripts/GameSetupController.cs
@@ -7,12 +7,17 @@ public class GameSetupController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreatePlayer();
+        CreateScout();
     }
 
-    private void CreatePlayer()
+    private void CreateScout()
     {
         Debug.Log("Loading Player.");
         PhotonNetwork.Instantiate(Path.Combine("Prefabs", "ScoutTankPrefab"), Vector3.up*10, Quaternion.identity);
+    }
+    private void CreateMortar()
+    {
+        Debug.Log("Loading Player.");
+        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "ScoutTank(2Part)"), Vector3.up*10, Quaternion.identity);
     }
 }
