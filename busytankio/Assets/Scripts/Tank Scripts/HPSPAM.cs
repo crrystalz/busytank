0aec7949e90ca2b94dfae8b02fe2e938eea91ae3
Mytheral <quinnkass@gmail.com>
2021-09-03 21:30:01 -0700

    jees

--- busytankio/Assets/Scripts/Tank Scripts/HPSPAM.cs
+++ busytankio/Assets/Scripts/Tank Scripts/HPSPAM.cs
@@ -235,7 +235,7 @@ public class HPSPAM : MonoBehaviour
                 PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Death Marker Variant"), pos, Quaternion.identity);
                 PhotonNetwork.Destroy(gameObject);
                 PhotonNetwork.Instantiate(Path.Combine("Prefabs", "CamCam"), Vector3.zero, Quaternion.identity);
-                PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Explosion2"), pos, Quaternion.identity);
+                PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Exploson2"), pos, Quaternion.identity);
             }
         }
        
